8dc30057889b49fa60a94d6757df3f139aa47697
Dmitryi Burykin <zer66607@gmail.com>
2018-02-17 22:52:35 +0300

    add help message, added handling of missed messages

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 
-from token import token
+from settings import token
 from config import start_msg
+from config import help_msg
 
 class zerBot:
 
@@ -10,7 +11,6 @@ class zerBot:
         self.api_url = 'https://api.telegram.org/bot{}/'.format(token)
         self.api_coin_url = 'https://api.cryptonator.com/api/ticker'
 
-    # Function get all updates in 24 hours
     def get_all_updates(self, offset = None, timeout = 30):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
@@ -18,18 +18,6 @@ class zerBot:
         result_json = resp.json()['result']
         return result_json
 
-    # Function get last update
-    def get_last_update(self):
-        results = self.get_all_updates()
-
-        if len(results) > 0:
-            last_update = results[-1]
-        else:
-            last_update = results[len(results)]
-
-        return last_update
-
-    # Function sending message
     def send_message(self, chat_id, message):
         method = 'sendMessage'
         params = {'chat_id': chat_id, 'text': message}
@@ -44,6 +32,7 @@ class zerBot:
 
 bot = zerBot(token)
 start = '/start'
+help = '/help'
 btc = '/btc'
 eth = '/eth'
 
@@ -51,30 +40,33 @@ def main():
     new_offset = None
     
     while True:
-        bot.get_all_updates(new_offset)
+        all_update = bot.get_all_updates(new_offset)
 
-        last_update = bot.get_last_update()
+        for last_update in all_update:
 
-        last_update_id = last_update['update_id']
-        print(last_update_id)
-        print(new_offset)
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_text = last_update['message']['text']
-        last_chat_name = last_update['message']['chat']['first_name']
+            last_update_id = last_update['update_id']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_text = last_update['message']['text']
+            last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text == start:
-            bot.send_message(last_chat_id, 'Приветствую тебя, ' + last_chat_name + start_msg)
+            if last_chat_text == start:
+                bot.send_message(last_chat_id, 'Приветствую тебя, ' + last_chat_name + start_msg)
 
-        elif last_chat_text == btc:
-            coin = bot.get_coin(btc)['price']
-            bot.send_message(last_chat_id, last_chat_name + ', курс Биткойна равен $' + coin)
+            elif last_chat_text == btc:
+                coin = float(bot.get_coin(btc)['price'])
+                coin = "${0:.3f}".format(coin)
+                bot.send_message(last_chat_id, last_chat_name + ', курс Биткойна равен ' + coin)
 
-        elif last_chat_text == eth:
-            coin = bot.get_coin(eth)['price']
-            bot.send_message(last_chat_id, last_chat_name + ', курс Эфириума равен $' + coin)
+            elif last_chat_text == eth:
+                coin = float(bot.get_coin(eth)['price'])
+                coin = "${0:.3f}".format(coin)
+                bot.send_message(last_chat_id, last_chat_name + ', курс Эфириума равен ' + coin)
 
-        else:
-            bot.send_message(last_chat_id, last_chat_name + ', к сожалению, таких команд я не понимаю!')
+            elif last_chat_text == help:
+                bot.send_message(last_chat_id, help_msg)
+
+            else:
+                bot.send_message(last_chat_id, last_chat_name + ', к сожалению, таких команд я не понимаю!')
 
         new_offset = last_update_id + 1
 
